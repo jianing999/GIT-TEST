6e9e375fd76a962b7f6ccae793b75dc185f91ba9
jianing <3154483501@qq.com>
2023-02-02 09:48:37 +0800

    third version

--- src/main/java/TEST.java
+++ src/main/java/TEST.java
@@ -6,5 +6,6 @@ public class TEST {
     public static void main(String[] args) {
         System.out.println("first version");
         System.out.println("second version");
+        System.out.println("third version");
     }
 }
